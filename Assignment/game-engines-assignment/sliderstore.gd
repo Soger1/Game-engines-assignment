b52d45910ca8d17771527a61752540d59cdeca9e
Soger1 <C20322553@mytudublin.ie>
2024-12-13 14:00:33 +0000

    added visual feedback from light

--- Assignment/game-engines-assignment/sliderstore.gd
+++ Assignment/game-engines-assignment/sliderstore.gd
@@ -2,6 +2,7 @@ extends Node
 
 var pitch_slider: float = 0.0
 var vib_slider:float =0.0
+var current_pitch:float =0.0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,3 +19,6 @@ func set_vib(value: float):
 	
 func set_pitch(value: float):
 	pitch_slider = value
+
+func set_cur_pitch(value: float):
+	current_pitch = value
